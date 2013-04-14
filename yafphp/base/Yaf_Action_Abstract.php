a7732078f0c9a685a591f4cf7c0797efb228b37b
zmrnet <zmrnet@qq.com>
2013-04-14 17:22:22 +0800

    @ yafphp

--- yafphp/base/Yaf_Action_Abstract.php
+++ yafphp/base/Yaf_Action_Abstract.php
@@ -1,4 +1,5 @@
 <?php
-abstract class Yaf_Action_Abstract extends Yaf_Action_Controller {
-	public abstract void execute ( void );
+abstract class Yaf_Action_Abstract extends Yaf_Action_Controller
+{
+	public abstract function execute();
 }
