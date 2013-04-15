b9dc45a111c5d5fdf312fcc652f75a73f6d0842c
zmrnet <zmrnet@qq.com>
2013-04-15 11:52:47 +0800

    @ yafphp

---
+++ yafphp/request/Yaf_Request_Http.php
@@ -0,0 +1,129 @@
+<?php
+// +----------------------------------------------------------------------
+// | yafphp [ Yaf PHP Framework ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://yafphp.duapp.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: baoqiang <zmrnet@qq.com>
+// +----------------------------------------------------------------------
+
+final class Yaf_Request_Http extends Yaf_Request_Abstract
+{
+	public function __construct($request_uri = NULL , $base_uri = NULL)
+	{
+
+	}
+
+	public function getLanguage ()
+	{
+
+	}
+
+	public function getQuery ($name = NULL )
+	{
+
+	}
+
+	public function getPost ($name = NULL )
+	{
+
+	}
+
+	public function getEnv ($name = NULL )
+	{
+
+	}
+
+	public function getServer ( $name = NULL )
+	{
+
+	}
+
+	public function getCookie ( $name = NULL )
+	{
+
+	}
+
+	public function getFiles ( $name = NULL )
+	{
+
+	}
+
+	public function isGet ()
+	{
+
+	}
+	
+	public function isPost ()
+	{
+
+	}
+	
+	public function isHead ()
+	{
+
+	}
+	
+	public function isXmlHttpRequest ()
+	{
+
+	}
+	
+	public function isPut ()
+	{
+
+	}
+	
+	public function isDelete ()
+	{
+
+	}
+	
+	public function isOption ()
+	{
+
+	}
+	
+	public function isCli ()
+	{
+
+	}
+	
+	public function isDispatched ()
+	{
+
+	}
+	
+	public function setDispatched ()
+	{
+
+	}
+	
+	public function isRouted ()
+	{
+
+	}
+	
+	public function setRouted ()
+	{
+
+	}
+	
+	public function getBaseUri ()
+	{
+
+	}
+	
+	public function setBaseUri ( $base_uri )
+	{
+
+	}
+	
+	public function getRequestUri ()
+	{
+
+	}
+	
+}
