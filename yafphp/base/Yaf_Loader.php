4e63e248969b05be73b92fa4f1aeabaecaa9f59c
zmrnet <zmrnet@qq.com>
2013-04-20 01:30:16 +0800

    @ yafphp

--- yafphp/base/Yaf_Loader.php
+++ yafphp/base/Yaf_Loader.php
@@ -18,14 +18,91 @@ final class Yaf_Loader
 	protected $_local_ns;
 
 	/**
+	 * __construct
+	 * 
+	 * @param void
+	 */
+	private function __construct()
+	{
+
+	}
+
+	/**
+	 * __clone
+	 * 
+	 * @param void
+	 */
+	private function __clone()
+	{
+		
+	}
+
+	/**
+	 * __sleep
+	 * 
+	 * @param void
+	 */
+	private function __sleep()
+	{
+		
+	}
+
+	/**
+	 * __wakeup
+	 * 
+	 * @param void
+	 */
+	private function __wakeup()
+	{
+		
+	}
+
+	/**
+	 * autoload
+	 *
+	 * @param string $class_name
+	 * @return 
+	 */
+	public function autoload($class_name)
+	{
+		if (!is_string($class_name)) {
+			return false;
+		}
+
+		do {
+			if (empty($class_name)){
+				break;
+			}
+			
+			if (YAF_USE_NAMESPACE) {
+				$origin_lcname = $class_name;
+				// class name with namespace in PHP 5.3
+				if (strpos($class_name, '\\') !== false) {
+					$class_name = str_replace('\\', '_', $class_name);
+				}
+			}
+
+			if (strncmp($class_name, 'Yaf_', 3) == 0) {
+				trigger_error('You should not use \'Yaf_\' as class name prefix', E_USER_WARNING);
+			}
+
+			
+		} while(0);
+
+	}
+	
+	/**
 	 * getInstance
 	 *
+	 * @param string $library
+	 * @param string $global_library
+	 * @return Yaf_Loader
 	 */
-	public static function getInstance($local_library_directory = null, $global_library_directory = null)
+	public static function getInstance($library = null, $global_library = null)
 	{
 		if (!(self::$_instance instanceof self)) {
-			if ((is_null($local_library_directory) && is_null($global_library_directory))
-					|| (empty($local_library_directory) && empty($global_library_directory))
+			if ((is_null($library) && is_null($global_library))
+					|| (empty($library) && empty($global_library))
 				){
 				return;
 			}
@@ -33,12 +110,12 @@ final class Yaf_Loader
 			self::$_instance = new self();
 		}
 		
-		if ($local_library_directory && is_string($local_library_directory)) {
-			self::$_instance->_library_directory = $local_library_directory;
+		if ($library && is_string($library)) {
+			self::$_instance->_library_directory = $library;
 		}
 
-		if ($global_library_directory && is_string($global_library_directory)) {
-			self::$_instance->_global_library_directory = $global_library_directory;
+		if ($global_library && is_string($global_library)) {
+			self::$_instance->_global_library_directory = $global_library;
 		}
 		return self::$_instance;
 	}
@@ -46,51 +123,90 @@ final class Yaf_Loader
 	/**
 	 * registerLocalNamespace
 	 *
+	 * @param mixed $namespace
+	 * @return boolean | Yaf_Loader
 	 */
 	public function registerLocalNamespace($namespace)
 	{
+		if (is_string($namespace)) {
+			if ($local_ns = YAF_G('local_namespaces')) {
+				YAF_G('local_namespaces', $local_ns . '/' . $namespace);
+			} else {
+				YAF_G('local_namespaces', $namespace);
+			}
+			return $this;
+		} elseif(is_array($namespace)) {
+			$local_ns = array();
+			foreach ($namespace as $key => $value) {
+				if (is_string($value)) {
+					$local_ns[] = $value;
+				}
+			}
+			YAF_G('local_namespaces', implode('/', $local_ns));
+			return $this;
+		}
 
+		return false;
 	}
 	
 	/**
 	 * getLocalNamespace
 	 *
+	 * @param void
+	 * @return string
 	 */
 	public function getLocalNamespace()
 	{
-
+		if ($local_ns = YAF_G('local_namespaces')) {
+			return (string)$local_ns;
+		}
+		return null;
 	}
 	
 	/**
 	 * clearLocalNamespace
 	 *
+	 * @param void
+	 * @return boolean
 	 */
 	public function clearLocalNamespace()
 	{
-
+		YAF_G('local_namespaces', null);
+		return true;
 	}
 	
 	/**
 	 * isLocalName
 	 *
+	 * @param string $class_name
+	 * @return boolean
 	 */
 	public function isLocalName($class_name)
 	{
+		if (!is_string($class_name)) {
+			return false;
+		}
 
-	}
-	
-	/**
-	 * autoload
-	 *
-	 */
-	public function autoload($class_name)
-	{
+		if (!($local_ns = YAF_G('local_namespaces'))) {
+			return false;
+		}
 
+		if (($pos = strpos($class_name, '_')) !== false) {
+			$prefix = substr($class_name, 0, $pos - 1);
+		} elseif(YAF_USE_NAMESPACE && ($pos = strpos($class_name, '\\')) !== false) {
+			$prefix = substr($class_name, 0, $pos - 1);
+		} else{
+			$prefix = $class_name;
+		}
+
+		return in_array($prefix, explode('/', $local_ns));
 	}
-	
+
 	/**
 	 * import
 	 *
+	 * @param string $file_name
+	 * @return boolean
 	 */
 	public static function import($file_name)
 	{
@@ -101,4 +217,35 @@ final class Yaf_Loader
 		return false;
 	}
 	
+	/**
+	 * setLibraryPath
+	 *
+	 * @param string $path
+	 * @param boolean $global
+	 * @return Yaf_Loader
+	 */
+	public static function setLibraryPath($path, $global = false)
+	{
+		if (!$global) {
+			$this->_library_directory = $path;
+		} else {
+			$this->_global_library_directory = $path;
+		}
+	}
+
+	/**
+	 * getLibraryPath
+	 *
+	 * @param boolean $global
+	 * @return string
+	 */
+	public static function getLibraryPath($global = false)
+	{
+		if (!$global) {
+			return $this->_library_directory;
+		} else {
+			return $this->_global_library_directory;
+		}
+	}
+
 }
