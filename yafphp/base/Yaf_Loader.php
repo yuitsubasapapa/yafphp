3041a490602a49f74fc0c408462decb14956ff9d
zmrnet <zmrnet@qq.com>
2013-04-21 17:55:00 +0800

    @ yafphp

--- yafphp/base/Yaf_Loader.php
+++ yafphp/base/Yaf_Loader.php
@@ -69,6 +69,10 @@ final class Yaf_Loader
 			return false;
 		}
 
+		$separator_len = strlen(YAF_NAME_SEPARATOR);
+		$app_directory = YAF_G('directory');
+		$origin_classname = $class_name;
+
 		do {
 			if (empty($class_name)){
 				break;
@@ -86,8 +90,79 @@ final class Yaf_Loader
 				trigger_error('You should not use \'Yaf_\' as class name prefix', E_USER_WARNING);
 			}
 
-			
+			if ($this->_is_category($class_name, 'Model')) {
+				/* this is a model class */
+				$directory = $app_directory . '/models';
+				
+				if (YAF_NAME_SUFFIX) {
+					$file_name = substr($class_name, 0, -(5 + $separator_len));
+				} else {
+					$file_name = substr($class_name, 5 + $separator_len);
+				}
+
+				break;
+			}
+
+			if ($this->_is_category($class_name, 'Plugin')) {
+				/* this is a plugin class */
+				$directory = $app_directory . '/plugins';
+
+				if (YAF_NAME_SUFFIX) {
+					$file_name = substr($class_name, 0, -(6 + $separator_len));
+				} else {
+					$file_name = substr($class_name, 6 + $separator_len);
+				}
+
+				break;
+			}
+
+			if ($this->_is_category($class_name, 'Controller')) {
+				/* this is a controller class */
+				$directory = $app_directory . '/controllers';
+
+				if (YAF_NAME_SUFFIX) {
+					$file_name = substr($class_name, 0, -(10 + $separator_len));
+				} else {
+					$file_name = substr($class_name, 10 + $separator_len);
+				}
+
+				break;
+			}
+
+/* {{{ This only effects internally */
+			if (YAF_G('st_compatible') && (strncmp($class_name, 'Dao_', 4) == 0
+						|| strncmp($class_name, 'Service_', 8) == 0)) {
+				/* this is a model class */
+				$directory = $app_directory . '/models';
+			}
+/* }}} */
+
 		} while(0);
+
+		if (!$app_directory && $directory) {
+			trigger_error('Couldn\'t load a framework MVC class without an Yaf_Application initializing', E_USER_WARNING);
+			return false;
+		}
+
+		if (!YAF_USE_SPL_AUTOLOAD) {
+			/** directory might be NULL since we passed a NULL */
+			if ($this->_internal_autoload($file_name, $directory)) {
+				if (class_exists($origin_classname, false)) {
+					return true;
+				} else {
+					trigger_error('Could not find class ' . $class_name . ' in ' . $directory, E_USER_WARNING);
+				}
+			}  else {
+				trigger_error('Failed opening script ' . $directory . ':' . YAF_ERR_AUTOLOAD_FAILED, E_USER_WARNING);
+			}
+			return true;
+		} else {
+			if ($this->_internal_autoload($file_name, $directory) &&
+					class_exists($origin_classname, false)) {
+				return true;
+			}
+			return false;
+		}
 
 	}
 	
@@ -248,4 +323,73 @@ final class Yaf_Loader
 		}
 	}
 
+	/**
+	 * yaf_loader_is_category
+	 *
+	 * @param string $class
+	 * @param string $category
+	 * @return boolean
+	 */
+	private function _is_category($class, $category)
+	{
+		$class_len = strlen($class);
+		$category = YAF_NAME_SUFFIX ? YAF_NAME_SEPARATOR . $category : $category . YAF_NAME_SEPARATOR;
+		$category_len = strlen($category);
+
+		if (YAF_NAME_SUFFIX) {
+			if ($class_len > $category_len && strncmp(substr($class, -$category_len), $category, $category_len) == 0) {
+				return true;
+			}
+		} else {
+			if (strncmp($class, $category, $category_len) == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * yaf_internal_autoload
+	 *
+	 * @param string $file_name
+	 * @param string $directory
+	 * @param string $file_path
+	 * @return boolean
+	 */
+	private function _internal_autoload($file_name, $directory = null, &$file_path = null)
+	{
+		if (is_null($directory)) {
+			$loader = Yaf_Loader::getInstance();
+			if (!$loader) {
+				/* since only call from userspace can cause loader is NULL, exception throw will works well */
+				trigger_error('Yaf_Loader need to be initialize first', E_USER_WARNING);
+				return false;
+			} else {
+				if ($loader->isLocalName($file_name)) {
+					$library_path = $loader->getLibraryPath();
+				} else {
+					$library_path = $loader->getLibraryPath(true);
+				}
+			}
+
+			if (empty($library_path)) {
+				trigger_error('Yaf_Loader requires Yaf_Application(which set the library_directory) to be initialized first', E_USER_WARNING);
+				return false;
+			}
+		}
+
+		if (($pos = strpos($file_name, '_')) !== false) {
+			$file_name[$pos] = '/';
+		}
+
+		if (YAF_G('lowcase_path')) {
+			$file_name = strtolower($file_name);
+		}
+		
+		$file_path = $directory . '/' . $file_name . '.' . YAF_G('ext');
+
+		return Yaf_Loader::import($file_path);
+	}
+	
 }
